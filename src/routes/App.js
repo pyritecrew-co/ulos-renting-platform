f8efc0877ae70e32689654f19a2c924839c8503c
tezadaconnect <terrencecalzada@gmail.com>
2021-11-28 23:56:16 +0800

    Initial Commit

---
+++ src/routes/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MainRouter from "./router";
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Ulos Rental Platform</h1>
+      <MainRouter />
+    </React.Fragment>
+  );
+}
+
+export default App;
